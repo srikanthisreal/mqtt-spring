2a58a9b30089291c226d988370ca9816d6653cc0
Srikanth <pasupuleti.srikanth27@gmail.com>
2024-02-11 14:31:01 +0100

    Initial Commit.

---
+++ src/main/java/com/mqtt/mqttspring/MqttSpringApplication.java
@@ -0,0 +1,13 @@
+package com.mqtt.mqttspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqttSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MqttSpringApplication.class, args);
+	}
+
+}
