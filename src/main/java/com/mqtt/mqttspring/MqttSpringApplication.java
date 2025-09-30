cb04cd19d2311880d691567c11ceee4d37ea7747
coming up <srikanthisreal@gmail.com>
2025-09-30 16:24:14 +0530

    Update MqttSpringApplication.java with logging!

--- src/main/java/com/mqtt/mqttspring/MqttSpringApplication.java
+++ src/main/java/com/mqtt/mqttspring/MqttSpringApplication.java
@@ -1,13 +1,17 @@
 package com.mqtt.mqttspring;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MqttSpringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MqttSpringApplication.class, args);
-	}
+    private static final Logger log = LoggerFactory.getLogger(MqttSpringApplication.class);
 
+    public static void main(String[] args) {
+        SpringApplication.run(MqttSpringApplication.class, args);
+        log.info("MqttSpringApplication started successfully.");
+    }
 }
